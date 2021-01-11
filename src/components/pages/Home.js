48ac64e6ee9440487e09ce86e07649e6d4ffe31a
EmilyGMG <emily.maria2010@hotmail.com>
2021-01-10 21:38:27 -0300

    first commit

---
+++ src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Cards from '../Cards';
+import Footer from '../Footer';
+import HeroSection from '../HeroSection';
+
+export const Home = () => {
+  return (
+    <>
+      <HeroSection />
+      <Cards />
+      <Footer />
+    </>
+  )
+}
